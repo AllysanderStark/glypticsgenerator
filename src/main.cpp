ffbfecc20e7600be1ca89b0b271a8ff2f966bb42
Vladyslav Kupriienko <kupr22@gmail.com>
2020-04-19 15:32:09 +0300

    Add CGAL and attempt Poisson reconstruction

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,21 @@
 #include <chrono>
 #include <string>
 
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/Surface_mesh_default_triangulation_3.h>
+#include <CGAL/make_surface_mesh.h>
+#include <CGAL/Implicit_surface_3.h>
+#include <CGAL/IO/facets_in_complex_2_to_triangle_mesh.h>
+#include <CGAL/Poisson_reconstruction_function.h>
+#include <CGAL/property_map.h>
+#include <CGAL/IO/read_xyz_points.h>
+#include <CGAL/compute_average_spacing.h>
+#include <CGAL/Polygon_mesh_processing/distance.h>
+#include <boost/iterator/transform_iterator.hpp>
+#include <vector>
+#include <fstream>
+
 #include "helpers.h"
 #include "OgreApp.h"
 
@@ -39,6 +54,23 @@ enum class direction
 	to_color
 };
 
+// CGAL Types
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef Kernel::FT FT;
+typedef Kernel::Point_3 cgPoint;
+typedef Kernel::Vector_3 cgVector;
+typedef std::pair<cgPoint, cgVector> Point_with_normal;
+typedef CGAL::First_of_pair_property_map<Point_with_normal> Point_map;
+typedef CGAL::Second_of_pair_property_map<Point_with_normal> Normal_map;
+typedef Kernel::Sphere_3 Sphere;
+typedef std::vector<Point_with_normal> PointList;
+typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
+typedef CGAL::Poisson_reconstruction_function<Kernel> Poisson_reconstruction_function;
+typedef CGAL::Surface_mesh_default_triangulation_3 STr;
+typedef CGAL::Surface_mesh_complex_2_in_triangulation_3<STr> C2t3;
+typedef CGAL::Implicit_surface_3<Kernel, Poisson_reconstruction_function> Surface_3;
+
+int reconstruct_mesh();
 void capture_frame(rs2::pipeline pipe);
 void generate_mesh(PointCloud<PointXYZ>::Ptr cloud);
 void show_mesh();
@@ -50,6 +82,8 @@ OgreApp app;
 
 frontal_face_detector detector = get_frontal_face_detector();
 shape_predictor predictor;
+
+PointList poisson_points;
 
 int main(int argc, char** argv) try
 {
@@ -133,6 +167,7 @@ int main(int argc, char** argv) try
 
 		dlib_win.add_overlay(render_face_detections(shapes));
 		point p;
+		poisson_points.clear();
 		for (auto face : shapes) {
 			for (unsigned long i = 0; i < face.num_parts(); i++) {
 				p = face.part(i);
@@ -141,17 +176,22 @@ int main(int argc, char** argv) try
 				// If there is no depth data for the landmark, try nearby pixels 
 				if (z == 0) {
 					for (int ax = -8; ax <= 8 && z == 0; ax++) {
-						for (int ay = -8; ay <= 8 && z == 0; ay++)
+						for (int ay = -8; ay <= 8 && z == 0; ay++) {
 							z = depth_frame.get_distance(p.x() + ax, p.y() + ay);
+						}
 					}
 				}
 				std::string z_label = (boost::format("%1$.2f") % z).str();
 				dlib_win.add_overlay(image_window::overlay_rect(rect, rgb_pixel(255, 0, 0), z_label));
+				auto n = cgVector(p.x(), p.y(), z - 3.0);
+				auto p_new = cgPoint(p.x(), p.y(), z);
+				auto pwn = Point_with_normal(p_new, n);
+				poisson_points.push_back(pwn);
 			}
 		}
 
 		dlib_win.set_image(cimg);
-
+		return reconstruct_mesh();
 
 		/*
 		glEnable(GL_BLEND);
@@ -204,6 +244,67 @@ catch (const std::exception & e)
 {
 	std::cerr << e.what() << std::endl;
 	return EXIT_FAILURE;
+}
+
+int reconstruct_mesh()
+{
+	// Poisson options
+	FT sm_angle = 20.0; // Min triangle angle in degrees.
+	FT sm_radius = 30; // Max triangle size w.r.t. point set average spacing.
+	FT sm_distance = 0.375; // Surface Approximation error w.r.t. point set average spacing.
+
+	Poisson_reconstruction_function function(poisson_points.begin(), poisson_points.end(), Point_map(), Normal_map());
+
+	// Computes the Poisson indicator function f()
+	// at each vertex of the triangulation.
+	if (!function.compute_implicit_function())
+		return EXIT_FAILURE;
+
+	// Computes average spacing
+	FT average_spacing = CGAL::compute_average_spacing<CGAL::Sequential_tag>
+		(poisson_points, 6 /* knn = 1 ring */,
+			CGAL::parameters::point_map(Point_map()));
+	// Gets one point inside the implicit surface
+	// and computes implicit function bounding sphere radius.
+	cgPoint inner_point = function.get_inner_point();
+	Sphere bsphere = function.bounding_sphere();
+	FT radius = std::sqrt(bsphere.squared_radius());
+	// Defines the implicit surface: requires defining a
+	// conservative bounding sphere centered at inner point.
+	FT sm_sphere_radius = 5.0 * radius;
+	FT sm_dichotomy_error = sm_distance * average_spacing / 1000.0; // Dichotomy error must be << sm_distance
+	Surface_3 surface(function,
+		Sphere(inner_point, sm_sphere_radius*sm_sphere_radius),
+		sm_dichotomy_error / sm_sphere_radius);
+	// Defines surface mesh generation criteria
+	CGAL::Surface_mesh_default_criteria_3<STr> criteria(sm_angle,  // Min triangle angle (degrees)
+		sm_radius*average_spacing,  // Max triangle size
+		sm_distance*average_spacing); // Approximation error
+	// Generates surface mesh with manifold option
+	STr tr; // 3D Delaunay triangulation for surface mesh generation
+	C2t3 c2t3(tr); // 2D complex in 3D Delaunay triangulation
+	CGAL::make_surface_mesh(c2t3,                                 // reconstructed mesh
+		surface,                              // implicit surface
+		criteria,                             // meshing criteria
+		CGAL::Manifold_with_boundary_tag());  // require manifold mesh
+	if (tr.number_of_vertices() == 0)
+		return EXIT_FAILURE;
+	// saves reconstructed surface mesh
+	std::ofstream out("poisson_mesh.off");
+	Polyhedron output_mesh;
+	CGAL::facets_in_complex_2_to_triangle_mesh(c2t3, output_mesh);
+	out << output_mesh;
+	// computes the approximation error of the reconstruction
+	double max_dist =
+		CGAL::Polygon_mesh_processing::approximate_max_distance_to_point_set
+		(output_mesh,
+			CGAL::make_range(boost::make_transform_iterator
+			(poisson_points.begin(), CGAL::Property_map_to_unary_function<Point_map>()),
+				boost::make_transform_iterator
+				(poisson_points.end(), CGAL::Property_map_to_unary_function<Point_map>())),
+			4000);
+	std::cout << "Max distance to point_set: " << max_dist << std::endl;
+	return EXIT_SUCCESS;
 }
 
 void capture_frame(rs2::pipeline pipe)
